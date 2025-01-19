53ec6d8ea9a97d935d3dba01effdb35af52e49b9
Scott Laird <scott@sigkill.org>
2025-01-19 11:02:32 -0800

    Working, added JSON output

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/scottlaird/vyos-parser
 
 go 1.22.2
+
+replace github.com/scottlaird/vyos-parser/xmlreader => ./xmlreader
+
+require github.com/scottlaird/vyos-parser/xmlreader v0.0.0-20250119183125-82a1aa2032f5
--- vyos-parser.go
+++ vyos-parser.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/xml"
+	"encoding/json"
 	"fmt"
-	"os"
-	"io/ioutil"
-	"path/filepath"
 	"github.com/scottlaird/vyos-parser/xmlreader"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	files, _ := ioutil.ReadDir(dir)
 
 	id := &xmlreader.InterfaceDefinition{}
-	
+
 	for _, file := range files {
 		if !file.IsDir() {
 			filename := filepath.Join(dir, file.Name())
@@ -22,21 +23,28 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			
+
 			interfacedef := &xmlreader.InterfaceDefinition{}
-			
+
 			err = xml.Unmarshal(b, &interfacedef)
 			if err != nil {
 				panic(err)
 			}
-			
+
 			//interfacedef.Print(0)
 
 			id.Merge(interfacedef)
 		}
 	}
 
+	id.Fixup()
+
 	fmt.Printf("********\n")
 	id.Print(0)
+	fmt.Printf("********\n")
+	b, err := json.MarshalIndent(id,"","  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
 }
-
