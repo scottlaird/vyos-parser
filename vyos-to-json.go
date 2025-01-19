e237e3f915c013cc9e126d2deb05c34926954ba3
Scott Laird <scott@sigkill.org>
2025-01-19 14:07:21 -0800

    Includes vyos-1x, makefile, and vyos-to-json

---
+++ vyos-to-json.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"github.com/scottlaird/vyos-parser/configmodel"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+var (
+	interfaceDefinitions = flag.String("interface-definitions", "vyos-1x/build/interface-definitions", "Source directory for interface XML files")
+	out                  = flag.String("out", "", "Output file")
+)
+
+func main() {
+	flag.Parse()
+	files, _ := ioutil.ReadDir(*interfaceDefinitions)
+
+	id := &configmodel.InterfaceDefinition{}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			filename := filepath.Join(*interfaceDefinitions, file.Name())
+			b, err := os.ReadFile(filename)
+			if err != nil {
+				panic(err)
+			}
+
+			interfacedef := &configmodel.InterfaceDefinition{}
+
+			err = xml.Unmarshal(b, &interfacedef)
+			if err != nil {
+				panic(err)
+			}
+
+			//interfacedef.Print(0)
+
+			id.Merge(interfacedef)
+		}
+	}
+
+	id.Fixup()
+
+	b, err := json.MarshalIndent(id, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	if out == nil || *out == "" {
+		fmt.Println(string(b))
+	} else {
+		err = os.WriteFile(*out, b, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
